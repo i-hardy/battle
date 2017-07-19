46d3982d237ac2ebee83661968240acb4500e9c1
Tambi-Mat <caicaratambi@gmail.com>
2017-07-19 14:15:01 +0100

    created enternames_spec feature test and index.erb and play.erb view files

--- app.rb
+++ app.rb
@@ -2,9 +2,17 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  run! if app_file == $0
+
   get '/' do
-    'Testing infrastructure working!'
+    # 'Testing infrastructure working!' Not sure if we still need this??
+    erb :index
   end
 
-  run! if app_file == $0
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
 end
---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,14 @@
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Dave'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+    expect(page).to have_content 'Dave vs. Mittens'
+  end
+end
+
+# Note that we are using Capybara's feature and scenario syntax. These work just
+# like describe and it, and we can use them interchangeably. This is because
+# Capybara-RSpec is simply a bunch of helpful methods on top of RSpec. Capybara
+# itself is simply a way of driving devices, like browsers, in a programmatic way.
